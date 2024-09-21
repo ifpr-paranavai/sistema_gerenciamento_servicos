9904be4b5f24a6dced427e882ab515733856f251
Santyero <santyero@gmail.com>
2024-09-21 02:44:56 -0300

    Correção Serializer de Usuario

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from authentication.models import User
+from core.api.serializers import FeatureSerializer
 from core.models.role import Role
         
 class UserSerializer(serializers.ModelSerializer):
@@ -7,10 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
         queryset=Role.objects.all(),
         slug_field='id'
     )
+    features = FeatureSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'password', 'cpf', 'role',]
+        fields = ['id', 'email', 'name', 'password', 'cpf', 'role', 'features']
         extra_kwargs = {
             'senha': {'write_only': True},
         }
@@ -19,5 +21,5 @@ class UserSerializer(serializers.ModelSerializer):
         roles_data = validated_data.pop('role')
         user = User.objects.create(**validated_data)
         user.role = roles_data
-        # user.features.set(role_data.features.all())
+        user.features.set(roles_data.features.all())
         return user
